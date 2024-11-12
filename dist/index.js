9b9410a2e78cec904d18eda2416b2dde87f44501
Christopher Diab <chrisdiab710@gmail.com>
2024-11-11 23:28:10 -0500

    added Tailwind

---
+++ dist/index.js
@@ -0,0 +1,108 @@
+//State
+const toDoList = {
+  name: "list",
+  createdAt: new Date(),
+  tasks: [
+    {
+      id: 1,
+      taskName: "task1",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      taskName: "task2",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      taskName: "task3",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      taskName: "task4",
+      isCompleted: false,
+    },
+    {
+      id: 5,
+      taskName: "task5",
+      isCompleted: false,
+    },
+  ],
+};
+
+//Modify State
+
+function addTask(taskName) {
+  const newTask = {
+    id: toDoList.tasks.length + 1,
+    taskName: taskName,
+    isCompleted: false,
+    createdAt: new Date(),
+  };
+  toDoList.tasks.push(newTask);
+  renderToDoList;
+}
+
+function deleteTask(toDoList, taskId) {
+  toDoList.tasks = toDoList.tasks.filter((task) => task.id !== taskId);
+  return toDoList;
+}
+
+function markTaskAsCompleted(toDoList, taskId) {
+  toDoList.tasks = toDoList.tasks.map((task) => {
+    if (task.id === taskId) {
+      task.isCompleted = true;
+    }
+    return task;
+  });
+  return toDoList;
+}
+
+//Render State
+function renderToDoList(toDoList) {
+  const listContainer = document.getElementById("task-list");
+  listContainer.innerHTML = "";
+  toDoList.tasks.forEach((task) => {
+    const taskElement = document.createElement("li");
+    taskElement.setAttribute(
+      "class",
+      "class = hover:bg-green-700 text-black font-bold py-2 px-4 rounded"
+    );
+    taskElement.innerText = task.taskName;
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute(
+      "class",
+      "class = hover:bg-red-700 text-black font-bold py-2 px-4 rounded"
+    );
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      deleteTask(toDoList, task.id);
+      renderToDoList(toDoList);
+    });
+    taskElement.appendChild(deleteButton);
+    listContainer.appendChild(taskElement);
+  });
+}
+
+renderToDoList(toDoList);
+
+//Script
+const addButton = document.getElementById("add-button");
+addButton.addEventListener("click", () => {
+  const taskName = prompt("Enter task name:");
+  const taskDate = prompt("Enter task date:");
+
+  if (taskName) {
+    addTask(taskName);
+    renderToDoList(toDoList);
+  }
+});
+
+//write a function that sets a timer for each task of 30 minutes
+const timer = (task) => {
+  setTimeout(() => {
+    alert(`Time is up for ${task.taskName}`);
+  }, 1000 * 60 * 30);
+};
+console.log(timer(toDoList.tasks[0]));
